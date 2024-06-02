24f3fc84f7d132420954160b2b550bcd05623b5b
orlandoroxo <orlandosilvaroxo@gmail.com>
2024-06-02 16:46:14 -0300

    readme finished

---
+++ README.md
@@ -0,0 +1,21 @@
+# Projeto Mesa Pronta
+
+Este projeto web para reservas de mesas de um restaurante conta com backend separado do frontend. Sendo um backend em PHP e um frontend em html, css e javascript.
+
+## Pré-requisitos de compilação e execução
+
+* Xampp Control Panel V.3.3.0
+* PHP Cli 8.3.7
+
+## Instruções de compilação e execução
+
+* Antes de compilar e executar, certifique-se de que o `Apache` e o banco de dados `MySQL`estejam em execução através do XAMPP Panel.
+* Com o banco de dados `MySQL`, execute os scripts SQL localizados em `database_scripts.sql`.
+* Feito os passos acima, é necessário apenas executar o backend.
+
+## Para compilar e executar:
+
+Execute o seguinte comando no terminal no diretório ../data
+```
+php -S localhost:8000
+```
--- mesa-pronta-back/database_scripts.sql
+++ mesa-pronta-back/database_scripts.sql
@@ -1,3 +1,10 @@
+-- Cria o banco de dados
+CREATE DATABASE estacio;
+
+-- Seleciona o banco de dados para uso
+USE estacio;
+
+-- Cria a tabela 'mesas'
 CREATE TABLE mesas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     coordenadas VARCHAR(255) NOT NULL,
@@ -5,12 +12,14 @@ CREATE TABLE mesas (
     status VARCHAR(255) NOT NULL
 );
 
+-- Cria a tabela 'users'
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username, password) VALUES ('user1', '123.');
+-- Insere usuários administradores
+INSERT INTO users (username, password) VALUES ('user1', '123');
 INSERT INTO users (username, password) VALUES ('user2', '123');
 INSERT INTO users (username, password) VALUES ('user3', '123');
